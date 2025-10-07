8036779053ff24e195e3a2d71c4d2619ccfa1fbb
Devlous <154725964+Devlous@users.noreply.github.com>
2025-10-07 13:23:22 +0100

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   const projects = [
     { title: "Portfolio Website", description: "Built with Next.js & Tailwind CSS", link: "https://my-portfolio-t4mw-ay48r1ed3-watchman1.vercel.app/projects#" },
     { title: "E-commerce front-end design", description: "React + GraphQL data visualizations", link: "https://github.com/Devlous/Akira-site" },
-    { title: "Weather App", description: "Next.js + OpenWeather API integration", link: "#" },
+    // { title: "Weather App", description: "Next.js + OpenWeather API integration", link: "#" },
   ];
 
   return (
